e8337df019966a6ed636a2d5241c5d25df763a60
Wesley Andrade Decezere <75259871+wesleydecezere@users.noreply.github.com>
2022-01-15 23:42:31 +0000

    Add final first attempt

--- faixa-branca/nivel-4/8-2436-robo-piso/main.py
+++ faixa-branca/nivel-4/8-2436-robo-piso/main.py
@@ -1,20 +1,59 @@
 l, c = list(map(int, input().split()))
-x, y = list(map(int, input().split()))
+y, x = list(map(int, input().split()))
+x -= 1
+y -= 1
+
 
 mtx = []
 
 for _ in range(l):
   mtx.append(input().split())
  
-  # define nro de direcoes
+dList = [
+  (1, 0),   # direita
+  (0, 1),   # para baixo
+  (-1, 0),  # esquerda
+  (0, -1)   # para cima
+] 
 
-  # enquanto houver direcao
-    # se houver 1 nessa direcao E se nao for contratia a direcao inicial
-      # movimenta na direcao
-      # (re)define direcao inicial
-      # redefine nro de direcoes
-    # se nao
-      # muda de direcao
-      # diminui nro de direcoes
+# define a direcao incial
+dInit = (0, 0)
 
-print(mtx)
+# define nro de direcoes
+dLen = len(dList)
+d = 0
+
+# enquanto houver direcao
+while (d < dLen):
+  dx = dList[d][0]
+  dy = dList[d][1]
+  
+  nx = x + dx
+  ny = y + dy
+
+  # (se nao atingir as bordas) E 
+  # (se houver 1 nessa direcao) E 
+  # (se ela nao for contratia a direcao inicial)
+  if (nx < len(mtx[0]) and ny < len(mtx)):
+    if (mtx[ny][nx] == '1'):
+      if ((dInit[0] != -dx or dx == 0) and (dInit[1] != -dy or dy == 0)):
+        # movimenta na direcao
+        x = nx
+        y = ny
+        
+        # (re)define direcao inicial
+        dInit = dList[d]
+
+        # redefine nro de direcoes
+        d = 0
+      else:
+        d += 1
+    else: 
+      d += 1
+  # se nao
+  else:
+    # muda de direcao
+    # diminui nro de direcoes
+    d += 1
+
+print(y + 1, x + 1)
---
+++ git.md
@@ -0,0 +1,2 @@
+# References
+1. [Delete branch locally and remotelly](https://www.freecodecamp.org/news/how-to-delete-a-git-branch-both-locally-and-remotely/)
