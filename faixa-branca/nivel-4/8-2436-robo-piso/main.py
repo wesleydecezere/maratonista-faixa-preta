2e1b1d97643070fc5ab4b2c5d8c8b92c26805df1
Wesley Andrade Decezere <75259871+wesleydecezere@users.noreply.github.com>
2022-01-15 16:58:00 +0000

    Add algoritmo

--- faixa-branca/nivel-4/8-2436-robo-piso/main.py
+++ faixa-branca/nivel-4/8-2436-robo-piso/main.py
@@ -5,5 +5,16 @@ mtx = []
 
 for _ in range(l):
   mtx.append(input().split())
+ 
+  # define nro de direcoes
+
+  # enquanto houver direcao
+    # se houver 1 nessa direcao E se nao for contratia a direcao inicial
+      # movimenta na direcao
+      # (re)define direcao inicial
+      # redefine nro de direcoes
+    # se nao
+      # muda de direcao
+      # diminui nro de direcoes
 
 print(mtx)
