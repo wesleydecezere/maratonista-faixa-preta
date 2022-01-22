b8f98c84c91234e8809440e4d6aa8e5e1c07c406
WesleyDecezere <>
2022-01-22 17:12:33 +0000

    Add f1-n4-e5 solution

---
+++ faixa-branca/nivel-4/5-1870-matriz-quadrada/main.py
@@ -0,0 +1,27 @@
+while(1):
+  try:
+    n = int(input())
+  except EOFError:
+    print()
+    break
+
+  init = int(n / 3)
+
+  m = [[]]
+  for i in range(n):
+    m.append([])
+    for j in range(n):
+      m[i].append('0')
+    
+      if i == j:
+        m[i][j] = '2'
+      if i + j == n-1:
+        m[i][j] = '3'
+      if (i >= init and i <= n-1-init) and (j >= init and j <= n-1-init):
+        m[i][j] = '1'
+      if i == j and i == int(n/2):
+        m[i][j] = '4'
+
+
+    print(''.join(m[i]))
+  print()
