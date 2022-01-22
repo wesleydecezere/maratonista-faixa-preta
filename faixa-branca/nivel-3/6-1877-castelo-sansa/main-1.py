579c742ce8e937ff3a677503f03aee65498dda45
WesleyDecezere <>
2022-01-22 16:15:21 +0000

    Add another algorithm

--- faixa-branca/nivel-3/6-1877-castelo-sansa/main-1.py
+++ faixa-branca/nivel-3/6-1877-castelo-sansa/main-1.py
@@ -10,21 +10,16 @@ grow = True
 
 for i in range(n):
     if i > 0:
-        if h[i] > h[i-1]:
+        if h[i] > h[i - 1]:
             if grow == False:
                 vale += 1
                 grow = True
-        elif h[i] < h[i-1]:
+        elif h[i] < h[i - 1]:
             if grow == True:
                 pico += 1
                 grow = False
-        else:
-            pico = 0
-            break
 
-
-
-if pico == k and vale == k-1 and grow == False:
+if pico == k and grow == False:
     print('beautiful')
 else:
     print('ugly')
---
+++ faixa-branca/nivel-3/6-1877-castelo-sansa/main-2.py
@@ -0,0 +1,15 @@
+n, k = input().split()
+n = int(n)
+k = int(k)
+
+h = input().split()
+
+pico = 0
+
+for i in range(1, n):
+  if h[i] > h[i - 1] and h[i] > h[i + 1]:
+    pico += 1
+
+out = "beautiful" if pico == k else "ugly"
+
+print(out)
