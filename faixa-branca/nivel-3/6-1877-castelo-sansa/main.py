553aadf79174e0e561c5074ad76a87807a525211
WesleyDecezere <>
2022-01-22 15:22:44 +0000

    Add first attempt

---
+++ faixa-branca/nivel-3/6-1877-castelo-sansa/main.py
@@ -0,0 +1,30 @@
+n, k = input().split()
+n = int(n)
+k = int(k)
+
+h = input().split()
+
+pico = 0
+vale = 0
+grow = True
+
+for i in range(n):
+    if i > 0:
+        if h[i] > h[i-1]:
+            if grow == False:
+                vale += 1
+                grow = True
+        elif h[i] < h[i-1]:
+            if grow == True:
+                pico += 1
+                grow = False
+        else:
+            pico = 0
+            break
+
+
+
+if pico == k and vale == k-1 and grow == False:
+    print('beautiful')
+else:
+    print('ugly')
