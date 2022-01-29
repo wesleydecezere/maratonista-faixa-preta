94b03ab8d4b99ac677c8ab6ba4280b260c877c7e
WesleyDecezere <>
2022-01-29 19:37:07 +0000

    Add algorithm, inputs

---
+++ faixa-amarela/nivel-1/02-1449-jaspion/main.cpp
@@ -0,0 +1,14 @@
+# para cada instancia
+  # para todos os pares de palavras
+    # key < input
+    # value < input
+    # dict += key, value
+
+  # para todas as linhas
+    # out = ''
+    # para cada palavras
+      # out += dict[palavra]
+    
+    # print(out)
+  # print()
+    
