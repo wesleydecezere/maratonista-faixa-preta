9427604a87ebfde34cfa6f395b0566075ef6e935
WesleyDecezere <>
2022-03-01 17:54:47 +0000

    Add impl 2 (file)

--- faixa-amarela/nivel-1/04-2091-numero-solitario/main.cpp
+++ faixa-amarela/nivel-1/04-2091-numero-solitario/main.cpp
@@ -6,11 +6,6 @@
 
 using namespace std;
 
-string vtos(vector<char> v) {
-  string s(v.begin(), v.end());
-  return s;
-}
-
 int main(int argc, char** argv) {
   ifstream input(argv[1]);  
   string inLine;
@@ -20,7 +15,6 @@ int main(int argc, char** argv) {
   vector<char> numbers = {};
   vector<char>::iterator it;
   char number;
-
   
   while(1) {
     getline(input, inLine);    
@@ -36,21 +30,17 @@ int main(int argc, char** argv) {
       numbers.end()
     );
       
-    while(1) {
-      cout << "Init: " << vtos(numbers) << endl;
-      
+    while(1) {     
       number = numbers[0];
       numbers.erase(numbers.begin());
 
       it = find(numbers.begin(), numbers.end(), number);
       
       if (it == numbers.end()) {
-        cout << "Out: " << number << endl;
+        cout << number << endl;
         break;
       }
 
-      cout << "Final: " << vtos(numbers) << endl; 
-      
       numbers.erase(it);
     }
   }
