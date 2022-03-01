129a451d7388bcb07a66fb12dad855cf91cf608c
WesleyDecezere <>
2022-03-01 17:52:58 +0000

    Debug impl 2

--- faixa-amarela/nivel-1/04-2091-numero-solitario/main.cpp
+++ faixa-amarela/nivel-1/04-2091-numero-solitario/main.cpp
@@ -1,38 +1,59 @@
 #include <iostream>
 #include <fstream>
 #include <algorithm>
+#include <vector>
+#include <bits/stdc++.h>
 
 using namespace std;
 
+string vtos(vector<char> v) {
+  string s(v.begin(), v.end());
+  return s;
+}
+
 int main(int argc, char** argv) {
+  ifstream input(argv[1]);  
   string inLine;
   char delimiter = ' ';  
 
   int n;
-  string numbers;
+  vector<char> numbers = {};
+  vector<char>::iterator it;
   char number;
 
-
+  
   while(1) {
-    getline(cin, inLine);    
+    getline(input, inLine);    
     n = stoi(inLine);
     if (n == 0) break;
 
-    getline(cin, numbers);
+    getline(input, inLine);
+    
+    numbers.clear();
+    copy(inLine.begin(), inLine.end(), std::back_inserter(numbers));
     numbers.erase(
       remove(numbers.begin(), numbers.end(), delimiter),
       numbers.end()
     );
-
-    while(1) {
-      number = numbers[0];
       
-      if (numbers.substr(1).find(number) == string::npos) {
-        cout << number << endl;
+    while(1) {
+      cout << "Init: " << vtos(numbers) << endl;
+      
+      number = numbers[0];
+      numbers.erase(numbers.begin());
+
+      it = find(numbers.begin(), numbers.end(), number);
+      
+      if (it == numbers.end()) {
+        cout << "Out: " << number << endl;
         break;
       }
 
-      numbers = numbers.substr(2);
+      cout << "Final: " << vtos(numbers) << endl; 
+      
+      numbers.erase(it);
     }
   }
+
+  input.close(); 
 }
